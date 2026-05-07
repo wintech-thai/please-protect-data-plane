f828e5e5654b78333aafa8af5478a0227245a208
pjames <pjames@gmail.com>
2026-05-07 19:47:10 +0700

    Moved GeoIP to logstash-transformer

--- 99-deployments/manifests/logstash-dispatcher/scripts/dispatcher.rb
+++ 99-deployments/manifests/logstash-dispatcher/scripts/dispatcher.rb
@@ -11,115 +11,9 @@ require 'redis'
 
 def register(params)
     $stdout.sync = true
-
-    redis_host = ENV['REDIS_HOST'] || 'localhost'
-
-    $redis = Redis.new(
-        host: redis_host,
-        port: 6379
-    )
 end
 
 def populate_ts_aggregate(event)
-end
-
-def public_ip?(ip)
-    return false if ip.nil? || ip.to_s.strip.empty?
-
-    begin
-        addr = IPAddr.new(ip)
-
-        # IPv4 checks
-        if addr.ipv4?
-            return false if addr.private?
-            return false if addr.loopback?
-            return false if addr.link_local?
-
-            # multicast
-            return false if IPAddr.new('224.0.0.0/4').include?(addr)
-
-            # reserved
-            return false if IPAddr.new('0.0.0.0/8').include?(addr)
-            return false if IPAddr.new('100.64.0.0/10').include?(addr)
-            return false if IPAddr.new('169.254.0.0/16').include?(addr)
-            return false if IPAddr.new('240.0.0.0/4').include?(addr)
-
-            return true
-        end
-
-        # IPv6 checks
-        if addr.ipv6?
-            return false if addr.loopback?
-            return false if addr.link_local?
-
-            # Unique local address (fc00::/7)
-            return false if IPAddr.new('fc00::/7').include?(addr)
-
-            # Multicast (ff00::/8)
-            return false if IPAddr.new('ff00::/8').include?(addr)
-
-            # Unspecified (::/128)
-            return false if IPAddr.new('::/128').include?(addr)
-
-            return true
-        end
-
-        false
-    rescue
-        false
-    end
-end
-
-def publish_geoip_attack_map(event)
-    source_ip   = event.get('[source][address]')
-    dest_ip     = event.get('[destination][address]')
-
-    source_port = event.get('[source][port]')
-    dest_port   = event.get('[destination][port]')
-
-    dataset     = event.get('[event][dataset]')
-
-    source_lat  = event.get('[source][geoip][latitude]')
-    source_long = event.get('[source][geoip][longitude]')
-
-    dest_lat    = event.get('[destination][geoip][latitude]')
-    dest_long   = event.get('[destination][geoip][longitude]')
-
-    source_country = event.get('[source][geoip][country_name]')
-    dest_country = event.get('[destination][geoip][country_name]')
-
-    # publish เฉพาะเมื่อ source หรือ destination เป็น public IP
-    return unless public_ip?(source_ip) || public_ip?(dest_ip)
-
-    payload = {
-        source_ip: source_ip,
-        dest_ip: dest_ip,
-        source_port: source_port,
-        dest_port: dest_port,
-        dataset: dataset,
-        source_lat: source_lat,
-        source_long: source_long,
-        dest_lat: dest_lat,
-        dest_long: dest_long,
-        source_country: source_country,
-        dest_country: dest_country,
-        ts: Time.now.to_i
-    }
-
-    begin
-        jsonStr = payload.to_json
-        puts("DEBUG : Publishing --> #{jsonStr}")
-
-        $redis.xadd(
-            'geoip-attack-map',
-            '*',
-            {
-                'data' => jsonStr
-            }
-        )
-    rescue => e
-        puts "Redis stream publish error: #{e}"
-    end
 end
 
 def filter(event)
@@ -128,8 +22,6 @@ def filter(event)
     event.set('cust_pod_name_dispatcher', ENV["POD_NAME"])
 
     line = event.get('message')
-
-    publish_geoip_attack_map(event)
 
     event.set('cust_ts_done_dispatched', Time.now.to_i)
     return [event]
--- 99-deployments/manifests/logstash-transformer/scripts/transformer.rb
+++ 99-deployments/manifests/logstash-transformer/scripts/transformer.rb
@@ -110,6 +110,105 @@ def populate_zone(event, from_field_name, to_field_name)
     event.set(to_field_name, '==unknown==')
 end
 
+def public_ip?(ip)
+    return false if ip.nil? || ip.to_s.strip.empty?
+
+    begin
+        addr = IPAddr.new(ip)
+
+        # IPv4 checks
+        if addr.ipv4?
+            return false if addr.private?
+            return false if addr.loopback?
+            return false if addr.link_local?
+
+            # multicast
+            return false if IPAddr.new('224.0.0.0/4').include?(addr)
+
+            # reserved
+            return false if IPAddr.new('0.0.0.0/8').include?(addr)
+            return false if IPAddr.new('100.64.0.0/10').include?(addr)
+            return false if IPAddr.new('169.254.0.0/16').include?(addr)
+            return false if IPAddr.new('240.0.0.0/4').include?(addr)
+
+            return true
+        end
+
+        # IPv6 checks
+        if addr.ipv6?
+            return false if addr.loopback?
+            return false if addr.link_local?
+
+            # Unique local address (fc00::/7)
+            return false if IPAddr.new('fc00::/7').include?(addr)
+
+            # Multicast (ff00::/8)
+            return false if IPAddr.new('ff00::/8').include?(addr)
+
+            # Unspecified (::/128)
+            return false if IPAddr.new('::/128').include?(addr)
+
+            return true
+        end
+
+        false
+    rescue
+        false
+    end
+end
+
+def publish_geoip_attack_map(event)
+    source_ip   = event.get('[source][address]')
+    dest_ip     = event.get('[destination][address]')
+
+    source_port = event.get('[source][port]')
+    dest_port   = event.get('[destination][port]')
+
+    dataset     = event.get('[event][dataset]')
+
+    source_lat  = event.get('[source][geoip][latitude]')
+    source_long = event.get('[source][geoip][longitude]')
+
+    dest_lat    = event.get('[destination][geoip][latitude]')
+    dest_long   = event.get('[destination][geoip][longitude]')
+
+    source_country = event.get('[source][geoip][country_name]')
+    dest_country = event.get('[destination][geoip][country_name]')
+
+    # publish เฉพาะเมื่อ source หรือ destination เป็น public IP
+    return unless public_ip?(source_ip) || public_ip?(dest_ip)
+
+    payload = {
+        source_ip: source_ip,
+        dest_ip: dest_ip,
+        source_port: source_port,
+        dest_port: dest_port,
+        dataset: dataset,
+        source_lat: source_lat,
+        source_long: source_long,
+        dest_lat: dest_lat,
+        dest_long: dest_long,
+        source_country: source_country,
+        dest_country: dest_country,
+        ts: Time.now.to_i
+    }
+
+    begin
+        jsonStr = payload.to_json
+        puts("DEBUG : Publishing --> #{jsonStr}")
+
+        @redisObj.xadd(
+            'geoip-attack-map',
+            '*',
+            {
+                'data' => jsonStr
+            }
+        )
+    rescue => e
+        puts "Redis stream publish error: #{e}"
+    end
+end
+
 def filter(event)
     populate_ts_aggregate(event)
     event.set('cust_ts_start_transformed', Time.now.to_i)
@@ -119,6 +218,8 @@ def filter(event)
 
     populate_zone(event, '[source][ip]', '[source][network_zone]')
     populate_zone(event, '[destination][ip]', '[destination][network_zone]')
+
+    publish_geoip_attack_map(event)
 
     event.set('cust_ts_done_transformed', Time.now.to_i)
     return [event]
