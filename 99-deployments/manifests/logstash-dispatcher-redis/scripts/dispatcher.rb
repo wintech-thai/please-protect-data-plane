159cb5385dd46402cfc8f147e44c5c13416ae176
Biobivv <phanuwat.p2002@gmail.com>
2026-07-04 18:37:52 +0700

    feat: add logstash dispatcher for Redis Streams with Elasticsearch integration

---
+++ 99-deployments/manifests/logstash-dispatcher-redis/scripts/dispatcher.rb
@@ -0,0 +1,26 @@
+require 'time'
+require 'date'
+require 'dalli'
+require 'net/http'
+require "json"
+require 'securerandom'
+require 'ipaddr'
+require 'csv'
+require 'nokogiri'
+require 'redis'
+
+def register(params)
+    $stdout.sync = true
+end
+
+def populate_ts_aggregate(event)
+end
+
+def filter(event)
+    populate_ts_aggregate(event)
+    event.set('cust_ts_start_dispatched', Time.now.to_i)
+    event.set('cust_pod_name_dispatcher', ENV["POD_NAME"])
+
+    event.set('cust_ts_done_dispatched', Time.now.to_i)
+    return [event]
+end
